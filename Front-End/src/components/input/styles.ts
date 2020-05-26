6800ed2d78a52e2af7363fa3507ddc1789a849e8
brenokf <breno-kf@hotmail.com>
2020-05-25 22:10:29 -0400

    implemetation interface -  exclude border/color

--- Front-End/src/components/input/styles.ts
+++ Front-End/src/components/input/styles.ts
@@ -1,13 +1,19 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   background: #232129;
   border-radius: 10px;
-  border: 2px solid #232129;
-  color: #666360;
   padding: 16px;
   width: 100%;
+
+  border: 2px solid #232129;
+  color: #666360;
 
   display: flex;
   align-items: center;
@@ -24,6 +30,20 @@ export const Container = styled.div`
       color: #666360;
     }
   }
+
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
+
   svg {
     margin-right: 16px;
   }

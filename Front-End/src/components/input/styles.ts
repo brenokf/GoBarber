563cd9f89265382c9deac89aaff3c80ebd061db4
brenokf <breno-kf@hotmail.com>
2020-05-26 22:00:13 -0400

    import Toolip and IsErrored

--- Front-End/src/components/input/styles.ts
+++ Front-End/src/components/input/styles.ts
@@ -1,9 +1,12 @@
 import styled, { css } from 'styled-components';
 import { shade } from 'polished';
 
+import Tooltip from '../Tooltip';
+
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -30,6 +33,11 @@ export const Container = styled.div<ContainerProps>`
       color: #666360;
     }
   }
+  ${props =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
 
   ${props =>
     props.isFocused &&
@@ -44,7 +52,27 @@ export const Container = styled.div<ContainerProps>`
       color: #ff9000;
     `}
 
+
+
+
   svg {
     margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;

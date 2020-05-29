12dffb3997674278c1efc1a1f9f7f146c6c130f6
brenokf <breno-kf@hotmail.com>
2020-05-29 10:33:42 -0400

    import authprovider and ToastConatiner

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -3,9 +3,16 @@ import GlobalStyle from './styles/global';
 import SignIn from './pages/Signin';
 import SingnUp from './pages/SignUp';
 
+import { AuthProvider } from './hooks/AuthContext';
+import ToastContainer from './components/ToastContainer';
+
 const App: React.FC = () => (
   <>
-    <SingnUp />
+    <AuthProvider>
+      <SignIn />
+    </AuthProvider>
+    <ToastContainer />
+
     <GlobalStyle />
   </>
 );

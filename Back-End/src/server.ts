f37efd75bbca67b88686b29e76eed3688fc6211b
brenokf <breno-kf@hotmail.com>
2020-05-29 10:33:02 -0400

    add cors

--- Back-End/src/server.ts
+++ Back-End/src/server.ts
@@ -1,21 +1,21 @@
-
 import 'reflect-metadata';
-import express , { Request, Response , NextFunction} from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+
 import 'express-async-errors';
 import routes from './routes/index';
 import './database';
 import uploadConfig from './config/upload';
 import AppError from './errors/AppError';
 
-
-
 const app = express();
-
+app.use(cors());
 app.use(express.json());
-app.use('/files',express.static(uploadConfig.directory));
+
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
-app.use((err:Error, request:Request, response:Response ,_:NextFunction)=> {
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
       status: 'error',
@@ -29,6 +29,6 @@ app.use((err:Error, request:Request, response:Response ,_:NextFunction)=> {
   });
 });
 
-app.listen(3333, ()=>{
+app.listen(3333, () => {
   console.log('🚀 Server started on port: 3333!');
-})
+});

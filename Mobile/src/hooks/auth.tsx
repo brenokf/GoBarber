87fb71a44698fbf3a6dc21ec437f7d0ea132149b
brenokf <breno-kf@hotmail.com>
2020-10-14 23:12:51 -0400

    insert state loading

--- Mobile/src/hooks/auth.tsx
+++ Mobile/src/hooks/auth.tsx
@@ -20,6 +20,7 @@ interface SingInCredentials {
 
 interface AuthContextData {
   user: object;
+  loading: boolean;
   signIn(credentials: SingInCredentials): Promise<void>;
   signOut(): void;
 }
@@ -28,7 +29,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
-  const [ loading, setLoading] = useState(true); // função para retirar
+  const [loading, setLoading] = useState(true); // função para retirar o tempo overflow
 
   useEffect(() => {
     async function loadStorageData(): Promise<void> {
@@ -39,6 +40,7 @@ const AuthProvider: React.FC = ({ children }) => {
       if (token[1] && user[1]) {
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
+      setLoading(false);
     }
 
     loadStorageData();
@@ -66,7 +68,7 @@ const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider value={{ user: data.user, loading, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );

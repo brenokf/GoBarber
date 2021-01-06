7d54db406cee16e5b740799d54324ec18b14bad6
brenokf <breno-kf@hotmail.com>
2021-01-06 11:29:43 -0400

    delet getRepository and add IUserRepository

--- Back-End/src/modules/users/services/AuthenticateUserService.ts
+++ Back-End/src/modules/users/services/AuthenticateUserService.ts
@@ -1,25 +1,27 @@
-import{ getRepository } from 'typeorm';
+
 import User from '@modules/users/infra/typeorm/entities/User';
 import {compare}from 'bcryptjs';
 import {sign, verify} from 'jsonwebtoken';
 import authConfig from '@config/auth';
 import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUserRepository';
 
-interface Request{
+interface IRequest{
   email:string;
   password:string;
 };
 
-interface Response{
+interface IResponse{
   user: User,
   token:string,
 }
 
 class AuthenticateUserService {
-  public async execute({email, password}:Request): Promise<Response>{
-    const userRepository = getRepository(User);
+  constructor(private userRepository: IUsersRepository){}
+  public async execute({email, password}:IRequest): Promise<IResponse>{
 
-    const user = await userRepository.findOne({ where:{ email } });
+
+    const user = await this.userRepository.findByEmail(email)
 
     if(!user){
       throw new AppError('Incorrect email /password combination',401);

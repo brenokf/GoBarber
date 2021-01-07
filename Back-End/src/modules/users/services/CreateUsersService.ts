5a14975248d74be109ce6aab2795c4390bb70826
brenokf <breno-kf@hotmail.com>
2021-01-06 20:11:35 -0400

    insert inject and injectable

--- Back-End/src/modules/users/services/CreateUsersService.ts
+++ Back-End/src/modules/users/services/CreateUsersService.ts
@@ -3,14 +3,20 @@ import User from '@modules/users/infra/typeorm/entities/User';
 import {hash}from 'bcryptjs';
 import AppError from '@shared/errors/AppError';
 import IUserRepository from '../repositories/IUserRepository';
+import {inject, injectable} from 'tsyringe'
+
 interface IRequest {
   name: string;
   email: string;
   password: string;
 }
-
+@injectable()
 class CreateUserService {
-  constructor(private userRepository: IUserRepository){}
+  constructor(
+    @inject('UsersRepository')
+    private userRepository: IUserRepository
+    ){}
+    
   public async execute({ name, email, password }:IRequest): Promise<User>{
     const checkUserExists = await this.userRepository.findByEmail(email);
 

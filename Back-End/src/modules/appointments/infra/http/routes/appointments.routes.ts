f0f1672fc85c804875f0bc6118ea992f94d009d6
brenokf <breno-kf@hotmail.com>
2021-01-06 20:05:40 -0400

    insert apointmentscontrollher in appointments routes

--- Back-End/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ Back-End/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,45 +1,14 @@
 import { Router, request, response } from 'express';
-import {parseISO} from 'date-fns';
+import AppointmentsController from '../controllers/AppointmentsController';
 
-import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+
 import ensureAuthenticated from '@shared/infra/http/middlewares/ensureAuthenticated';
 
-
-
-
+const appointmentsController = new AppointmentsController();
 const appointmentsRouter = Router();
-
 
 appointmentsRouter.use(ensureAuthenticated);
 
-
-// appointmentsRouter.get('/',async ( request, response)=>{
-
-//   const appointments = await appointmentsRepository.find();
-
-//   return response.json(appointments);
-// });
-
-
-appointmentsRouter.post('/', async (request, response) => {
-
-  const {provider_id,date} = request.body;
-
-  const parsedDate = parseISO(date);
-  const appointmentsRepository = new AppointmentsRepository();
-  const createAppointment = new CreateAppointmentService(
-    appointmentsRepository,
-
-    );
-
-  const appointment = await createAppointment.execute({
-    date: parsedDate,
-    provider_id,
-  });
-
-  return response.json(appointment);
-
-});
+appointmentsRouter.post('/', appointmentsController.create) ;
 
 export default appointmentsRouter;

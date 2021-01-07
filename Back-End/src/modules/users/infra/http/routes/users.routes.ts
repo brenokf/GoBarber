7acb53dc993cf44695c9c7063263dbf0408bbaa6
brenokf <breno-kf@hotmail.com>
2021-01-06 20:10:29 -0400

    remove create userSErvice and userRepository

--- Back-End/src/modules/users/infra/http/routes/users.routes.ts
+++ Back-End/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,48 +1,21 @@
 import { Router, request, response } from 'express';
-import CreateUserService from '@modules/users/services/CreateUsersService';
+
 import multer from 'multer';
 import uploadConfig from '@config/upload';
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UserRepository';
 import UpadateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
-
 import ensureAuthenticated from '@shared/infra/http/middlewares/ensureAuthenticated';
+
+import UserAvatarController from '../../controllers/UserAvatarController';
+import UsersController from '../../controllers/UsersController';
+
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
+const userController  = new UsersController();
+const userAvatarController  = new UserAvatarController();
 
+usersRouter.post('/', userController.create );
 
-usersRouter.post('/', async (request, response) => {
-
-    const { name, email, password } = request.body;
-    
-    const userRepository = new UsersRepository();
-    const createUser = new CreateUserService(userRepository);
-
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
-    });
-
-    delete user.password;
-
-    return response.json(user);
-
-});
-
-usersRouter.patch('/avatar',ensureAuthenticated,upload.single('avatar'), async (request, response) => {
-
-    const updateUserAvatar = new UpadateUserAvatarService(userRepository);
-
-   const user = await updateUserAvatar.execute({
-      user_id: request.user.id,
-      avatarFilename:request.file.filename,
-    });
-
-    delete user.password;
-    return response.json(user);
-
-
-});
+usersRouter.patch('/avatar',ensureAuthenticated,upload.single('avatar'),userAvatarController.update);
 
 export default usersRouter;

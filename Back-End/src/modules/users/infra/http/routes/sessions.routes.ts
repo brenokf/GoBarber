8291dd3aa1394f266b1f4e35ba634494be8ab0e0
brenokf <breno-kf@hotmail.com>
2021-01-06 20:09:49 -0400

    remove usersREpository and transpor m/ post

--- Back-End/src/modules/users/infra/http/routes/sessions.routes.ts
+++ Back-End/src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,24 +1,10 @@
 import { Router } from 'express';
 
-import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UserRepository';
+import SessionController from '../../controllers/SessionsController';
+
 
 const sessionsRouter = Router();
+const sessionController = new SessionController();
 
-sessionsRouter.post('/', async (request, response) => {
-
-    const { email, password} = request.body;
-    const userRepository = new UsersRepository();
-    const authenticateUser = new AuthenticateUserService(userRepository);
-
-   const { user,token } = await authenticateUser.execute({
-      email,
-      password,
-    });
-    delete user.password;
-
-
-    return response.json({ user, token });
-
-});
+sessionsRouter.post('/',sessionController.create);
 export default sessionsRouter;

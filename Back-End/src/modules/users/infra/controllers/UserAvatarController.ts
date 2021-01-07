e2134ec45b4f654cd622475dc843fbc80eef0f22
brenokf <breno-kf@hotmail.com>
2021-01-06 20:08:32 -0400

    create UserAvatarCONTROLLER

---
+++ Back-End/src/modules/users/infra/controllers/UserAvatarController.ts
@@ -0,0 +1,19 @@
+import {container} from 'tsyringe';
+import { Request, Response } from 'express';
+import UpadateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+
+export default class UserAvatarController {
+
+  public async update( request:Request, response:Response): Promise<Response>{
+    const updateUserAvatar = container.resolve(UpadateUserAvatarService);
+
+    const user = await updateUserAvatar.execute({
+       user_id: request.user.id,
+       avatarFilename:request.file.filename,
+     });
+
+     delete user.password;
+     return response.json(user);
+
+  }
+}

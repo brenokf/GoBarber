5c1c699be5cf69718431b8fcfb9e205c92a1d23d
brenokf <breno-kf@hotmail.com>
2021-01-06 20:12:51 -0400

    construct inject dependecies

--- Back-End/src/shared/container/index.ts
+++ Back-End/src/shared/container/index.ts
@@ -1,3 +1,19 @@
 import {container} from 'tsyringe';
 
-import
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
+
+import IUsersRepository from '@modules/users/repositories/IUserRepository';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UserRepository';
+
+container.registerSingleton<IAppointmentsRepository>(
+  'AppointmentsRepository',
+  AppointmentsRepository
+);
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository
+);
+
+

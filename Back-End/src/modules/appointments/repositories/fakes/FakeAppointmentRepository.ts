742ce07ffa49da8adff7746c19409d79702073e1
brenokf <breno-kf@hotmail.com>
2021-01-08 21:25:10 -0400

    include isEqual depencie

--- Back-End/src/modules/appointments/repositories/fakes/FakeAppointmentRepository.ts
+++ Back-End/src/modules/appointments/repositories/fakes/FakeAppointmentRepository.ts
@@ -1,18 +1,18 @@
 
 import IAppointmentsRepository from "@modules/appointments/repositories/IAppointmentsRepository";
 import Appointment from "@modules/appointments/infra/typeorm/entities/Appointment";
-
-import {uuid} from 'uuidv4';
+import {isEqual} from 'date-fns';
+import {fromString, uuid} from 'uuidv4';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 
 
 
-class AppointmentsRepository implements IAppointmentsRepository {
+class FakeAppointmentRepository implements IAppointmentsRepository {
   private appointments: Appointment[] = [];
 
   public async findByDate(date:Date): Promise<Appointment | undefined> {
     const findAppointment = this.appointments.find(
-      appointment => appointment.date === date
+      appointment => isEqual(appointment.date,date)
       );
     return findAppointment;
 
@@ -29,4 +29,4 @@ public async create({provider_id,date}:ICreateAppointmentDTO): Promise<Appointme
 }
 }
 
-export default AppointmentsRepository;
+export default FakeAppointmentRepository;

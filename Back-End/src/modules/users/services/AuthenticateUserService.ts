9c4cf6dc77346daef55583998876cbe2dc8e28b8
brenokf <breno-kf@hotmail.com>
2021-01-08 23:25:04 -0400

    include reflect-metadata and IHashProvider

--- Back-End/src/modules/users/services/AuthenticateUserService.ts
+++ Back-End/src/modules/users/services/AuthenticateUserService.ts
@@ -1,10 +1,13 @@
+import 'reflect-metadata'
 import {inject, injectable} from 'tsyringe'
 import User from '@modules/users/infra/typeorm/entities/User';
-import {compare}from 'bcryptjs';
 import {sign, verify} from 'jsonwebtoken';
 import authConfig from '@config/auth';
 import AppError from '@shared/errors/AppError';
+
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 import IUsersRepository from '../repositories/IUserRepository';
+
 
 interface IRequest{
   email:string;
@@ -20,6 +23,9 @@ class AuthenticateUserService {
   constructor(
     @inject('UsersRepository')
     private userRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider:IHashProvider,
     ){}
 
   public async execute({email, password}:IRequest): Promise<IResponse>{
@@ -31,7 +37,7 @@ class AuthenticateUserService {
       throw new AppError('Incorrect email /password combination',401);
     }
 
-    const passwordMatched = await compare(password, user.password);
+    const passwordMatched = await this.hashProvider.compareHash(password, user.password);
 
     if(!passwordMatched){
       throw new AppError('Incorrect email /password combination',401);
